bdd0978b1d6b18c5359323bc1246043cd279e807
feroz-istar <feroz@istarindia.com>
2018-01-07 17:57:16 +0530

    ccc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FieldErrorDisplayComponent } from './field-error-display/field-error-display.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FieldErrorDisplayComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl("", Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl("", Validators.compose([Validators.required, Validators.minLength(4)]))
+    });
+
+
+  }
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('form submitted');
+      this.router.navigate(['../dashboard'], { relativeTo: this.route });
+
+    } else {
+      console.log('form invalid');
+
+    }
+  }
+  isFieldValid(field: string) {
+    return !this.form.get(field).valid && this.form.get(field).touched;
   }
 
+  isValid(field: string) {
+    return this.form.get(field).valid && this.form.get(field).touched;
+  }
+
+  displayFieldCss(field: string) {
+    return {
+      'has-danger': this.isFieldValid(field),
+      'has-success': this.isValid(field)
+    };
+  }
 }
